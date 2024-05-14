b9b2db77d0dffadf9818419d804e53cce088b734
jonovdm <vdm.jono@gmail.com>
2024-05-14 14:38:16 +0100

    stream confusion

--- run.js
+++ run.js
@@ -1,85 +1,56 @@
 import { keccak256, toHex } from 'viem';
 import { HypersyncClient } from "@envio-dev/hypersync-client";
 
-const event_signatures = [
-    "PoolCreated(address,address,uint24,int24,address)",
-    "Burn(address,int24,int24,uint128,uint256)",
-    "Initialize(uint160,int24)",
-    "Mint(address,address,int24,int24,uint128,uint256,uint256)",
-    "Swap(address,address,int256,int256,uint160,uint128,int24)"
+//Please note this contract emits zero events
+const addresses = [
+  "0xeFE9a82d56cd965D7B332c7aC1feB15c53Cd4340".toLowerCase(),
 ];
-
-const topic0_list = event_signatures.map(sig => keccak256(toHex(sig)));
-
-console.log(topic0_list);
 
 const client = HypersyncClient.new({
   url: "http://eth.hypersync.xyz"
 });
 
+//Goal: I'm trying to get all transactions involving the contract above, so I can extract information from the data/input of these transactions
+//block used is a random block that contains a tx to the contract I care about: https://etherscan.io/tx/0xae381b28dadeb5120a8b5033958375c502fa8564e576569e29942b34f1e0214c
 let query = {
-    "fromBlock": 0,
-    "logs": [
-      {
-        // Get all events that have any of the topic0 values we want
-        "topics": [
-          topic0_list,
-        ]
-      }
-    ],
-    "fieldSelection": {
-      "block": [
-        "number",
-        "timestamp",
-        "hash",
-      ],
-      "log": [
-        "block_number",
-        "log_index",
-        "transaction_index",
-        "transaction_hash",
-        "data",
-        "address",
-        "topic0",
-        "topic1",
-        "topic2",
-        "topic3"
-      ],
-      "transaction": [
-        "from",
-      ]
-    },
+  "fromBlock": 19799508,
+  "transactions": [
+    {
+      to: addresses
+    }
+  ],
+  "fieldSelection": {
+    "transaction": [
+      "from",
+      "to",
+      "input"
+    ]
+  },
 };
 
 const main = async () => {
-  let eventCount = 0;
-  const startTime = performance.now()
-
+  let results = []
   // Send an initial non-parallelized request to find first events
   const res = await client.sendEventsReq(query);
-  eventCount += res.events.length;
+  results.push(res)
   query.fromBlock = res.nextBlock;
 
-  // Start streaming events in parallel
-  const stream = await client.streamEvents(query, { retry: true, batchSize: 10000, concurrency: 12 });
+  const stream = await client.stream(query, { retry: true, batchSize: 10000, concurrency: 12 });
 
-  while(true) {
+  while (true) {
     const res = await stream.recv();
-
     // Quit if we reached the tip
     if (res === null) {
       console.log(`reached the tip`);
       break;
     }
 
-    eventCount += res.events.length;
-
-    const currentTime = performance.now();
-
-    const seconds = (currentTime - startTime) / 1000;
-
-    console.log(`scanned up to ${res.nextBlock} and got ${eventCount} events. ${seconds} seconds elapsed. Events per second: ${eventCount / seconds}`);
+    // console.log(res)
+    console.log(`scanned up to ${res.nextBlock}`);
+    results.push(res)
   }
+  //MY PROBLEM: Why aren't the transactions being picked up?
+  console.log(results)
 };
 
 main();
